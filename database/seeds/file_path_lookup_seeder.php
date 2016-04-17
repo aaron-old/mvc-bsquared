58778c8e0f72ffdf640e65ee72efb174cc41f246
Aaron Young <mustarddevelopment@gmail.com>
2016-04-17 13:32:01 -0700

    Working through creating the home page.

---
+++ database/seeds/file_path_lookup_seeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class file_path_lookup_seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
