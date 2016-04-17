95d5083262fd016abf8a2c6ffab7178bbfd2fef8
Aaron Young <mustarddevelopment@gmail.com>
2016-04-17 04:06:13 -0700

    Adding the database migration files.

---
+++ database/migrations/2016_04_17_110228_create_portfolio_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePortfolioProfilesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('portfolio_profiles', function(Blueprint $table)
+		{
+			$table->integer('userID', true);
+			$table->string('firstName', 48)->nullable();
+			$table->string('lastName', 48)->nullable();
+			$table->text('aboutMe', 65535)->nullable();
+			$table->timestamp('created')->default(DB::raw('CURRENT_TIMESTAMP'));
+			$table->timestamp('modified')->default(DB::raw('CURRENT_TIMESTAMP'));
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('portfolio_profiles');
+	}
+
+}
